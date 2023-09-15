5b7850a37f62786a57bbcbe26557eab5e4107f0f
Gustavo Henning <gustavo.henning@outlook.com>
2023-09-15 14:26:27 +0200

    create service impl

--- src/main/java/com/gustavohenning/dbecommercev1/service/TokenService.java
+++ src/main/java/com/gustavohenning/dbecommercev1/service/TokenService.java
@@ -12,31 +12,7 @@ import org.springframework.stereotype.Service;
 import java.time.Instant;
 import java.util.stream.Collectors;
 
-@Service
-public class TokenService {
-
-    @Autowired
-    private JwtEncoder jwtEncoder;
-
-    @Autowired
-    private JwtDecoder jwtDecoder;
-
-    public String generateJwt(Authentication auth){
-
-        Instant now = Instant.now();
-
-        String scope = auth.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.joining(" "));
-
-        JwtClaimsSet claims = JwtClaimsSet.builder()
-                .issuer("self")
-                .issuedAt(now)
-                .subject(auth.getName())
-                .claim("roles", scope)
-                .build();
-
-        return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
-    }
+public interface TokenService {
+    public String generateJwt(Authentication auth);
 
 }
---
+++ src/main/java/com/gustavohenning/dbecommercev1/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,154 @@
+package com.gustavohenning.dbecommercev1.service.impl;
+
+import com.google.gson.*;
+import com.gustavohenning.dbecommercev1.entity.ApplicationUser;
+import com.gustavohenning.dbecommercev1.entity.Role;
+import com.gustavohenning.dbecommercev1.entity.dto.LoginResponseDTO;
+import com.gustavohenning.dbecommercev1.repository.RoleRepository;
+import com.gustavohenning.dbecommercev1.repository.UserRepository;
+import com.gustavohenning.dbecommercev1.service.AuthenticationService;
+import com.gustavohenning.dbecommercev1.service.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.net.URL;
+import java.net.URLConnection;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Transactional
+public class AuthenticationServiceImpl implements AuthenticationService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    public ApplicationUser registerUser(String username, String password, String name, String email, String postalCode, String state, String city, String neighborhood, String street) throws Exception {
+
+        String encodedPassword = passwordEncoder.encode(password);
+        Role userRole = roleRepository.findByAuthority("USER").get();
+
+        Set<Role> authorities = new HashSet<>();
+
+        authorities.add(userRole);
+
+        ApplicationUser userWithAddress = new ApplicationUser(0L, username, encodedPassword, name, email, postalCode, state, city, neighborhood, street, authorities);
+        getAddress(userWithAddress);
+
+        try {
+            getAddress(userWithAddress);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Invalid postal code");
+        }
+
+        return userRepository.save(userWithAddress);
+    }
+
+    public  ApplicationUser getAddress(ApplicationUser user) throws Exception {
+        String postalCode = user.getPostalCode();
+
+        if (postalCode.length() == 5) {
+            // API Zippopotamus
+            URL url = new URL("https://api.zippopotam.us/ES/" + postalCode);
+            URLConnection connection = url.openConnection();
+            InputStream is = connection.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+
+            StringBuilder jsonCep = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                jsonCep.append(line);
+            }
+            JsonElement rootElement = JsonParser.parseString(jsonCep.toString());
+            JsonObject jsonObject = rootElement.getAsJsonObject();
+
+            String city = jsonObject.getAsJsonArray("places").get(0).getAsJsonObject().get("place name").getAsString();
+            String state = jsonObject.getAsJsonArray("places").get(0).getAsJsonObject().get("state").getAsString();
+
+            user.setCity(city);
+            user.setState(state);
+
+            br.close();
+            is.close();
+
+        } else if (postalCode.length() == 8) {
+            // API CEP
+            URL url = new URL("https://brasilapi.com.br/api/cep/v1/" + postalCode);
+            URLConnection connection = url.openConnection();
+            InputStream is = connection.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+
+            String postCode = "";
+            StringBuilder jsonPostalCode = new StringBuilder();
+
+            while ((postCode = br.readLine()) != null) {
+                jsonPostalCode.append(postCode);
+            }
+            Gson gson = new GsonBuilder().registerTypeAdapter(LocalDateTime.class, new JsonDeserializer<LocalDateTime>() {
+                @Override
+                public LocalDateTime deserialize(JsonElement json, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+                    return ZonedDateTime.parse(json.getAsJsonPrimitive().getAsString()).toLocalDateTime();
+                }
+            }).create();
+
+            ApplicationUser userAux = gson.fromJson(jsonPostalCode.toString(), ApplicationUser.class);
+
+
+            user.setPostalCode(postalCode);
+            user.setState(userAux.getState());
+            user.setCity(userAux.getCity());
+            user.setNeighborhood(userAux.getNeighborhood());
+            user.setStreet(userAux.getStreet());
+
+            br.close();
+            is.close();
+
+        } else {
+            throw new IllegalArgumentException("Invalid postal code only BR and ES available");
+        }
+
+        return user;
+    }
+
+
+    public LoginResponseDTO loginUser(String username, String password){
+
+        try{
+            Authentication auth = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(username, password)
+            );
+
+            String token = tokenService.generateJwt(auth);
+
+            return new LoginResponseDTO(userRepository.findByUsername(username).get(), token);
+
+        } catch(AuthenticationException e){
+            return new LoginResponseDTO(null, "");
+        }
+    }
+
+}
