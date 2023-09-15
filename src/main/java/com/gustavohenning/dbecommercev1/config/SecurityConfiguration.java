1148957ae4d648e5bec9f229aff49aea1debf633
Gustavo Henning <gustavo.henning@outlook.com>
2023-09-15 14:58:02 +0200

    replace with lambda AbstractHttpConfigurer::disable in SecurityConfiguration

--- src/main/java/com/gustavohenning/dbecommercev1/config/SecurityConfiguration.java
+++ src/main/java/com/gustavohenning/dbecommercev1/config/SecurityConfiguration.java
@@ -17,6 +17,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -55,7 +56,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers(HttpMethod.GET, "/brand/**").permitAll();
                     auth.requestMatchers(HttpMethod.GET, "/category/**").permitAll();
